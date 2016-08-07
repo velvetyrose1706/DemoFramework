0cbfef956a25e407fcf07f4211bdd3c44556373c
NhungHuynh <nhunghuynhthihong@gmail.com>
2016-08-07 16:55:47 +0700

    [ADD] ^^

---
+++ Demo.swift
@@ -0,0 +1,17 @@
+//
+//  Demo.swift
+//  DemoFramework
+//
+//  Created by Nhung Huynh on 8/7/16.
+//  Copyright © 2016 Nhung Huynh. All rights reserved.
+//
+
+import Foundation
+
+
+public class Demo{
+    
+    public static func sayHi() {
+        print("HI there")
+    }
+}
---
+++ DemoFramework/DemoFramework.h
@@ -0,0 +1,19 @@
+//
+//  DemoFramework.h
+//  DemoFramework
+//
+//  Created by Nhung Huynh on 8/7/16.
+//  Copyright © 2016 Nhung Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DemoFramework.
+FOUNDATION_EXPORT double DemoFrameworkVersionNumber;
+
+//! Project version string for DemoFramework.
+FOUNDATION_EXPORT const unsigned char DemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoFramework/PublicHeader.h>
+
+
